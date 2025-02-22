03c2a2d53d4adcf625403cd81bc9575bf59386e9
Ayyum <qaiyyumkamal047@gmail.com>
2025-02-23 01:09:27 +0800

    Resume update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+/*
+// Google Analytics
+// Create GA Id
+(function() {
+    var script = document.createElement('script');
+    script.src = "https://www.googletagmanager.com/gtag/js?id=UA-XXXXX-X";
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = function() {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-XXXXX-X');
+    };
+})();
+*/
+
 let lastScrollTop = 0;
 const header = document.querySelector("header");
 
